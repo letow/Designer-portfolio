c37f5a19162a864ead7c9e569b084540dad41eb1
George <71167099+letow@users.noreply.github.com>
2022-01-05 02:00:30 +0500

    burger menu fixed

--- js/main.js
+++ js/main.js
@@ -32,6 +32,40 @@ var arrLang = {
       'contact-text': 'Хочешь узнать больше или просто поболтать?<br/>Пожалуйста!',
       'send': 'Отправить&nbsp;сообщение',
       'bottom-text': 'Лайкайте меня на<br/>LinkedIn, Instagram, Behance, Dribbble',
+    },
+    'en1': {
+      'home': 'Home',
+      'about': 'About me',
+      'skills': 'Skills',
+      'portfolio': 'Portfolio',
+      'contact': 'Contacts',
+      'denis': 'Denis<br/>Novik',
+      'occupation': 'UX | UI designer<br/>24 years old, Minsk',
+      'description': 'Hi, I&apos;m Denis – UX/UI designer from Minsk.<br>I&apos;m interested in design and everything connected with it.<br><br><br>I&apos;m studying at courses "Web and mobile design interfaces" in IT-Academy.<br><br><br>Ready to implement excellent projects<br>with wonderful people.',
+      'progs': 'I work in such programs as',
+      'fashion-store': 'Online fashion store - Homepage',
+      'reebok-store': 'Reebok Store - Concept',
+      'braun-landing': 'Braun Landing Page - Concept',
+      'contact-text': 'Want to know more or just chat?<br/>You are welcome!',
+      'send': 'Send&nbsp;Message',
+      'bottom-text': 'Like me on<br/>LinkedIn, Instagram, Behance, Dribbble',
+    },
+    'ru1': {
+      'home': 'Главная',
+      'about': 'Обо мне',
+      'skills': 'Навыки',
+      'portfolio': 'Портфолио',
+      'contact': 'Контакты',
+      'denis': 'Денис<br/>Новик',
+      'occupation': 'UX | UI дизайнер<br/>24 года, Минск',
+      'description': 'Привет, я Денис – UX/UI дизайнер из Минска.<br>Интересуюсь дизайном и всем, что с ним связано.<br><br><br>Учусь на курсе "Дизайн мобильных и веб-интерфейсов" в Академии АйТи.<br><br><br>Готов реализовывать отличные проекты<br/>с замечательными людьми.',
+      'progs': 'Я работаю в таких программах как:',
+      'fashion-store': 'Интернет-магазин одежды - Главная страница',
+      'reebok-store': 'Магазин Reebok - Концепт',
+      'braun-landing': 'Лэндинг Braun - Концепт',
+      'contact-text': 'Хочешь узнать больше или просто поболтать?<br/>Пожалуйста!',
+      'send': 'Отправить&nbsp;сообщение',
+      'bottom-text': 'Лайкайте меня на<br/>LinkedIn, Instagram, Behance, Dribbble',
     }
   }
   
@@ -45,4 +79,15 @@ var arrLang = {
     });
   });
 
-  
+
+  $(function() {
+    $('#menu-burger__toggle').change(function() {
+      if(document.getElementById('menu-burger__toggle').checked){
+        document.getElementById("lang-btn").style.visibility = "visible";
+      }
+      else{
+        document.getElementById("lang-btn").style.visibility = "hidden";
+      }
+    });
+  });
+
